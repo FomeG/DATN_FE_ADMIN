d109515c6cfbb6b79bb35a1da035ba55236b9f14
FomeG <nghiavuong04@gmail.com>
2025-04-18 02:52:52 +0700

    Cập nhật thêm voucherUI

---
+++ src/app/voucher/voucher-ui/voucher-ui.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-voucher-ui',
+  imports: [],
+  templateUrl: './voucher-ui.component.html',
+  styleUrl: './voucher-ui.component.css'
+})
+export class VoucherUiComponent {
+
+}
